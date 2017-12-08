bfbd9d15cda74ac28e273aae64bc2278050a2185
周方圆 <zhoufangyuan@hzdusun.com>
2017-12-08 15:45:56 +0800

    修改错误

--- app/src/main/java/peripherals/Sys.java
+++ app/src/main/java/peripherals/Sys.java
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
+package peripherals;
+
 /**
  * Created by Papillon on Nov 21, 2017.
  * "C:\Program Files\Java\jdk1.8.0_144\bin\javah.exe" -v -jni -classpath ./jni -d ./jni peripherals.LedHelper
  */
- 
-package peripherals; 
 
 
 public class Sys {
