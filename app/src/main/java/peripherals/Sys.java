7b5d3275941f92f2d3eb42ba471e219061f7f48f
wuliang <wy19941007>
2017-12-08 15:47:56 +0800

    新增JNI数据修改1111

--- app/src/main/java/peripherals/Sys.java
+++ app/src/main/java/peripherals/Sys.java
@@ -1,24 +1,22 @@
-<<<<<<< HEAD
 /**
  * Created by Papillon on Nov 21, 2017.
  * "C:\Program Files\Java\jdk1.8.0_144\bin\javah.exe" -v -jni -classpath ./jni -d ./jni peripherals.LedHelper
  */
- 
-package peripherals; 
+
+package peripherals;
 
 
 public class Sys {
     static {
-		try {
-			System.loadLibrary("peripherals");			
-		}
-		catch (UnsatisfiedLinkError e) {
-			System.out.println("Failed to load library: " + e);		
-			System.exit(-1);
-		}
+        try {
+            System.loadLibrary("peripherals");
+        } catch (UnsatisfiedLinkError e) {
+            System.out.println("Failed to load library: " + e);
+            System.exit(-1);
+        }
     }
-    
+
     public static native int rebootToRecovery();
-        
+
 }
 
