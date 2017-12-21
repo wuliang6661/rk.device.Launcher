aa2c6329ef5118a79539fb2904568431c4136508
wuliang <wy19941007>
2017-12-21 17:44:20 +0800

    。。。。。

--- app/src/main/java/rk/device/launcher/service/UsbBroadCastReceiver.java
+++ app/src/main/java/rk/device/launcher/service/UsbBroadCastReceiver.java
@@ -20,70 +20,72 @@ import static android.content.Context.STORAGE_SERVICE;
 
 
 public class UsbBroadCastReceiver extends BroadcastReceiver {
-	
-	public interface OnDecryptedListener {
-		void onDecryptedFinished(List<File> fileList);
-	}
-	
-	public void setOnDecryptedListener(OnDecryptedListener listener) {
-		mOnDecryptedListener = listener;
-	}
-	
-	private OnDecryptedListener mOnDecryptedListener;
-	
-	// 获取次存储卡路径,一般就是外置 TF 卡了. 不过也有可能是 USB OTG 设备...
-	// 其实只要判断第二章卡在挂载状态,就可以用了.
-	public String getSecondaryStoragePath(Context context) {
-		try {
-			StorageManager sm = (StorageManager) context.getSystemService(STORAGE_SERVICE);
-			Class<StorageManager> clazz = StorageManager.class;
-			Method getVolumePathsMethod = clazz.getMethod("getVolumePaths", new Class<?>[]{});
-			String[] paths = (String[]) getVolumePathsMethod.invoke(sm, new Object[]{});
-			// second element in paths[] is secondary storage path
-			return paths.length <= 2 ? null : paths[1];
-		} catch (Exception e) {
-			Log.e(TAG, "getSecondaryStoragePath() failed", e);
-		}
-		return null;
-	}
-	
-	private final String TAG = "UsbBroadCastReceiver";
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		// 当sd卡插上的时候
-		if (Intent.ACTION_MEDIA_MOUNTED.equals(intent.getAction())) {
-			// 外置tf卡的路径
+
+    public interface OnDecryptedListener {
+        void onDecryptedFinished(List<File> fileList);
+    }
+
+    public void setOnDecryptedListener(OnDecryptedListener listener) {
+        mOnDecryptedListener = listener;
+    }
+
+    private OnDecryptedListener mOnDecryptedListener;
+
+    // 获取次存储卡路径,一般就是外置 TF 卡了. 不过也有可能是 USB OTG 设备...
+    // 其实只要判断第二章卡在挂载状态,就可以用了.
+    public String getSecondaryStoragePath(Context context) {
+        try {
+            StorageManager sm = (StorageManager) context.getSystemService(STORAGE_SERVICE);
+            Class<StorageManager> clazz = StorageManager.class;
+            Method getVolumePathsMethod = clazz.getMethod("getVolumePaths", new Class<?>[]{});
+            String[] paths = (String[]) getVolumePathsMethod.invoke(sm, new Object[]{});
+            // second element in paths[] is secondary storage path
+            return paths.length <= 2 ? null : paths[1];
+        } catch (Exception e) {
+            Log.e(TAG, "getSecondaryStoragePath() failed", e);
+        }
+        return null;
+    }
+
+    private final String TAG = "UsbBroadCastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "sdcard in");
+        // 当sd卡插上的时候
+        if (Intent.ACTION_MEDIA_MOUNTED.equals(intent.getAction())) {
+            // 外置tf卡的路径
 //			String sdCardDirPath = "/mnt/external_sd";
-			String sdCardDirPath = getSecondaryStoragePath(context);
-			if (TextUtils.isEmpty(sdCardDirPath)) {
-				return;
-			}
-			File roombankerDir = new File(sdCardDirPath, "roombanker");
-			List<File> encryptedFileList = FileUtils.listFilesInDirWithFilter(roombankerDir, ".ao", true);
-			if (encryptedFileList == null || encryptedFileList.isEmpty()) {
-				return;
-			}
-			List<File> picFileList = new ArrayList<>();
-			for (File encryptedFile : encryptedFileList) {
-				// 获取全路径中的文件名
-				String fileName = FileUtils.getFileName(encryptedFile);
-				String destDirPath = "/data/rk_backup/rk_ad";
-				File destDir = new File(destDirPath);
-				if (FileUtils.createOrExistsDir(destDir)) {
-					File decryptedFile = new File(destDir, fileName + ".jpeg");
-					// 成功复制
-					if (FileUtil.encryptFile(encryptedFile, decryptedFile)) {
-						picFileList.add(decryptedFile);
-					}
-				}
-			}
-			// 复制完毕
-			Toast.makeText(context, "复制完毕", Toast.LENGTH_LONG).show();
-			if (mOnDecryptedListener != null && !picFileList.isEmpty()) {
-				mOnDecryptedListener.onDecryptedFinished(picFileList);
-			}
-		}
-		
-	}
+            String sdCardDirPath = getSecondaryStoragePath(context);
+            if (TextUtils.isEmpty(sdCardDirPath)) {
+                return;
+            }
+            File roombankerDir = new File(sdCardDirPath, "roombanker");
+            List<File> encryptedFileList = FileUtils.listFilesInDirWithFilter(roombankerDir, ".ao", true);
+            if (encryptedFileList == null || encryptedFileList.isEmpty()) {
+                return;
+            }
+            List<File> picFileList = new ArrayList<>();
+            for (File encryptedFile : encryptedFileList) {
+                // 获取全路径中的文件名
+                String fileName = FileUtils.getFileName(encryptedFile);
+                String destDirPath = "/data/rk_backup/rk_ad";
+                File destDir = new File(destDirPath);
+                if (FileUtils.createOrExistsDir(destDir)) {
+                    File decryptedFile = new File(destDir, fileName + ".jpeg");
+                    // 成功复制
+                    if (FileUtil.encryptFile(encryptedFile, decryptedFile)) {
+                        picFileList.add(decryptedFile);
+                    }
+                }
+            }
+            // 复制完毕
+            Toast.makeText(context, "复制完毕", Toast.LENGTH_LONG).show();
+            Log.d(TAG, "sdcard image copy to /data/rk_backup suress!!");
+            if (mOnDecryptedListener != null && !picFileList.isEmpty()) {
+                mOnDecryptedListener.onDecryptedFinished(picFileList);
+            }
+        }
+
+    }
 }
