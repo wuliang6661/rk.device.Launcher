4f8e380223d95e3dfd96e116144b900f25016e1a
wuliang <wy19941007>
2017-12-27 16:06:14 +0800

    所有jni方法添加注释文档

--- app/src/main/java/peripherals/NfcHelper.java
+++ app/src/main/java/peripherals/NfcHelper.java
@@ -6,6 +6,9 @@
 package peripherals; 
 
 
+/*
+ * @brief This class provides interface to native code of the NFC device.
+ */
 public class NfcHelper {
     static {
 		try {
@@ -16,11 +19,36 @@ public class NfcHelper {
 			System.exit(-1);
 		}
     }
-    
+
+	/*
+     * @brief Init the NFC device. It must be called before any other methods. 
+     * @return 0 for success, and non-zero for failure.
+     * @note The corresponding C prototype is:
+     * @code{.c}
+     * int PER_nfcInit();
+     * @endcode
+     */
     public static native int PER_nfcInit();
-        
+
+	/*
+     * @brief Deinit the NFC device.
+     * @return 0 for success, and non-zero for failure.
+     * @note The corresponding C prototype is:
+     * @code{.c}
+     * int PER_nfcDeinit();
+     * @endcode
+     */
     public static native int PER_nfcDeinit();
-    
+
+	/*
+     * @brief Get identified NFC card.
+     * @param card The identified NFC card. It will be filled by native code.
+     * @return 0 for success, and non-zero for failure.
+     * @note The corresponding C prototype is:
+     * @code{.c}
+     * int PER_nfcGetCard(rk_nfc_card_uid *pCard)
+     * @endcode
+     */
 	public static native int PER_nfcGetCard(NfcCard card);
 }
 
--- app/src/main/java/peripherals/NumberpadHelper.java
+++ app/src/main/java/peripherals/NumberpadHelper.java
@@ -1,25 +1,54 @@
-package peripherals;
-
 /**
  * Created by Papillon on Dec 22, 2017.
  */
+ 
+package peripherals; 
 
 
+/*
+ * @brief This class provides interface to native code of the number pad device.
+ */
 public class NumberpadHelper {
     static {
-        try {
-            System.loadLibrary("peripherals");
-        }
-        catch (UnsatisfiedLinkError e) {
-            System.out.println("Failed to load library: " + e);
-            System.exit(-1);
-        }
+		try {
+			System.loadLibrary("peripherals");			
+		}
+		catch (UnsatisfiedLinkError e) {
+			System.out.println("Failed to load library: " + e);		
+			System.exit(-1);
+		}
     }
 
+	/*
+     * @brief Init the number pad device. It must be called before any other methods. 
+     * @return 0 for success, and non-zero for failure.
+     * @note The corresponding C prototype is:
+     * @code{.c}
+     * int PER_numberpadInit();
+     * @endcode
+     */
     public static native int PER_numberpadInit();
 
+	/*
+     * @brief Deinit the number pad device.
+     * @return 0 for success, and non-zero for failure.
+     * @note The corresponding C prototype is:
+     * @code{.c}
+     * int PER_numberpadDeinit();
+     * @endcode
+     */
     public static native int PER_numberpadDeinit();
 
-    public static native int PER_numberpadPress(String keys);
+	/*
+     * @brief Pass user-pressed numbers to the number pad device.
+     * @param keys User-pressed numbers to be passed to the number pad device.
+     * @return 0 for success, and non-zero for failure.
+     * @note The corresponding C prototype is:
+     * @code{.c}
+     * int PER_numberpadPress(const char *pKeys)
+     * @endcode
+     */
+	public static native int PER_numberpadPress(String keys);
 }
+
 
