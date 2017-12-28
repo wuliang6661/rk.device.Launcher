803d11553677cea23c7110d8ffb49700fec3514d
hanbin <hanbin@xcvudeMacBook-Pro.local>
2017-12-28 14:41:32 +0800

    指纹，NFC相关类

---
+++ app/src/main/java/rk/device/launcher/utils/verify/VerifyUtils.java
@@ -0,0 +1,62 @@
+package rk.device.launcher.utils.verify;
+
+import java.util.List;
+
+import rk.device.launcher.db.DbHelper;
+import rk.device.launcher.db.entity.User;
+
+/**
+ * Created by hanbin on 2017/12/28.
+ * <p/>
+ * NFC，指纹等校验帮助类
+ */
+public class VerifyUtils {
+
+    private static VerifyUtils verifyUtils = null;
+
+    public static VerifyUtils getInstance() {
+        if (verifyUtils == null) {
+            synchronized (VerifyUtils.class) {
+                if (verifyUtils == null) {
+                    verifyUtils = new VerifyUtils();
+                }
+            }
+        }
+        return verifyUtils;
+    }
+
+    public VerifyUtils() {
+
+    }
+
+    /**
+     * NFC检验
+     * 
+     * @param nfcCard
+     * @return
+     */
+    public boolean verifyByNfc(String nfcCard) {
+        List<User> userList = DbHelper.queryByNFCCard(nfcCard);
+        if (userList.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 指纹检验
+     * 
+     * @param fingerId
+     * @return
+     */
+    public boolean verifyByFinger(int fingerId) {
+        List<User> userList = DbHelper.queryByFinger(fingerId);
+        if (userList.size() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
