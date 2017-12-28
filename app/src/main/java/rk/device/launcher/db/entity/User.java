4e464d79d7ef71e57240cb15b3a0e69ac8e44250
周方圆 <zhoufangyuan@hzdusun.com>
2017-12-28 09:45:45 +0800

    修改user字段

--- app/src/main/java/rk/device/launcher/db/entity/User.java
+++ app/src/main/java/rk/device/launcher/db/entity/User.java
@@ -9,50 +9,58 @@ import org.greenrobot.greendao.annotation.Generated;
 /**
  * Created by mundane on 2017/12/27 下午3:00
  */
-@Entity
+@Entity(generateGettersSetters = false)
 public class User {
 	@Id(autoincrement = true)
 	public Long id;
 	
 	@NotNull
 	@Index(unique = true)
-	public String uniqueId;
+	public String uniqueId; // 这就是那个唯一标识id
 	
 	@NotNull
 	public String name;
+	
+	@NotNull
+	public String popedomType;
+	
+	public String cardNo;
+	
+	public String fingerID;
+	
+	public String faceID;
+	
+	public int passWord;
+	
+	public long startTime;
+	
+	public long endTime;
+	
+	public String fingerCode;
+	
+	public int uploadStatus;
 
-	@Generated(hash = 1692395986)
-	public User(Long id, @NotNull String uniqueId, @NotNull String name) {
+	@Generated(hash = 1246737531)
+	public User(Long id, @NotNull String uniqueId, @NotNull String name,
+			@NotNull String popedomType, String cardNo, String fingerID, String faceID,
+			int passWord, long startTime, long endTime, String fingerCode,
+			int uploadStatus) {
 		this.id = id;
 		this.uniqueId = uniqueId;
 		this.name = name;
+		this.popedomType = popedomType;
+		this.cardNo = cardNo;
+		this.fingerID = fingerID;
+		this.faceID = faceID;
+		this.passWord = passWord;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.fingerCode = fingerCode;
+		this.uploadStatus = uploadStatus;
 	}
 
 	@Generated(hash = 586692638)
 	public User() {
 	}
 
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getUniqueId() {
-		return this.uniqueId;
-	}
-
-	public void setUniqueId(String uniqueId) {
-		this.uniqueId = uniqueId;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
