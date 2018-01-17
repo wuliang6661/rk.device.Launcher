9465e266a3ac2d2210012b1c85cafc1dede275ea
周方圆 <zhoufangyuan@hzdusun.com>
2018-01-17 14:28:12 +0800

    修改

--- app/src/main/java/rk/device/launcher/base/BaseActivity.java
+++ app/src/main/java/rk/device/launcher/base/BaseActivity.java
@@ -11,7 +11,6 @@ import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
