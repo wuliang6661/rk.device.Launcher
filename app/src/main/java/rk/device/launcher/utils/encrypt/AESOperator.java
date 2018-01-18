55ea50ac6e2a4ffd6ca326bada2ded89e8543e66
周方圆 <zhoufangyuan@hzdusun.com>
2018-01-18 15:30:30 +0800

    修改了搜索框字符长度输入限制, 修改了AES加密的命名

--- app/src/main/java/rk/device/launcher/utils/encrypt/AESOperator.java
+++ app/src/main/java/rk/device/launcher/utils/encrypt/AESOperator.java
@@ -11,8 +11,8 @@ import javax.crypto.spec.SecretKeySpec;
  * Created by mundane on 2017/12/28.
  */
 public class AESOperator {
-    private static String sKey = "0123456789abcdef"; //key，可自行修改
-    private static String ivParameter = "1020304050607080"; //偏移量,可自行修改
+    private static final String KEY = "0123456789abcdef"; // key，可自行修改
+    private static final String IV_PARAMETER = "1020304050607080"; // 偏移量,可自行修改, 必须是16位
 
 
     private static String getRandomString(int length) { //length表示生成字符串的长度
@@ -40,16 +40,16 @@ public class AESOperator {
     }
 
     // 加密
-    private static String encrypt(String sSrc) {
+    private static String encrypt(String source) {
 
         try {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            byte[] raw = sKey.getBytes();
+            byte[] raw = KEY.getBytes();
             SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-            IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());// 使用CBC模式，需要一个向量iv，可增加加密算法的强度
+            IvParameterSpec iv = new IvParameterSpec(IV_PARAMETER.getBytes());// 使用CBC模式，需要一个向量iv，可增加加密算法的强度
             cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
-            byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
-            return Base64Utils.encode(encrypted);// 此处使用BASE64做转码。
+            byte[] encryptedBytes = cipher.doFinal(source.getBytes("utf-8"));
+            return Base64Utils.encode(encryptedBytes);// 此处使用BASE64做转码。
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -57,14 +57,14 @@ public class AESOperator {
     }
 
     // 解密
-    private static String decrypt(String sSrc) {
+    private static String decrypt(String encryptedString) {
         try {
-            byte[] raw = sKey.getBytes("ASCII");
+            byte[] raw = KEY.getBytes("ASCII");
             SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            IvParameterSpec iv = new IvParameterSpec(ivParameter.getBytes());
+            IvParameterSpec iv = new IvParameterSpec(IV_PARAMETER.getBytes());
             cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
-            byte[] encrypted1 = Base64Utils.decode(sSrc);// 先用base64解密
+            byte[] encrypted1 = Base64Utils.decode(encryptedString);// 先用base64解密
             byte[] original = cipher.doFinal(encrypted1);
             String originalString = new String(original, "utf-8");
             return originalString;
