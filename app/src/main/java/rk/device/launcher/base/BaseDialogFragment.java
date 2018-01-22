51304ac5487f9a8a5f6710fa43e4cdb2d199e415
周方圆 <zhoufangyuan@hzdusun.com>
2018-01-22 17:06:52 +0800

    修改dialogfragment的休眠监听事件

--- app/src/main/java/rk/device/launcher/base/BaseDialogFragment.java
+++ app/src/main/java/rk/device/launcher/base/BaseDialogFragment.java
@@ -1,14 +1,17 @@
 package rk.device.launcher.base;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.MotionEvent;
+import android.view.KeyEvent;
 import android.view.View;
 
+import rk.device.launcher.R;
 import rk.device.launcher.service.SleepTaskServer;
 import rk.device.launcher.ui.setting.SleepActivity;
 import rk.device.launcher.utils.LogUtil;
+import rk.device.launcher.widget.CustomFrameLayout;
 
 /**
  * Created by wuliang on 2017/12/25.
@@ -16,27 +19,52 @@ import rk.device.launcher.utils.LogUtil;
  * 所有Fragment的共有父类
  */
 
-public class BaseDialogFragment extends DialogFragment {
+public class BaseDialogFragment extends DialogFragment implements CustomFrameLayout.OnFrameLayoutTouchListener, DialogInterface.OnKeyListener {
 
+    public CustomFrameLayout mCustomFrameLayout;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getView().setOnTouchListener((v, event) -> {
-            LogUtil.d("wuliang", " getView dialogFragment OnTouchListener");
-            if (getActivity() instanceof SleepActivity) {
-                return false;
-            }
-            SleepTaskServer.getSleepHandler(getActivity()).sendEmptyMessage(0x11);
-            return true;
-        });
-        getDialog().getWindow().getDecorView().setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                LogUtil.d("wuliang", "dialogFragment OnTouchListener");
-                return false;
-            }
-        });
+        mCustomFrameLayout = view.findViewById(R.id.custom_framelayout);
+        if (mCustomFrameLayout != null) {
+            mCustomFrameLayout.setOnFrameLayoutTouchListener(this);
+        }
+
+        getDialog().setOnKeyListener(this);
+//        getView().setOnTouchListener((v, event) -> {
+//            LogUtil.d("wuliang", " getView dialogFragment OnTouchListener");
+//            if (getActivity() instanceof SleepActivity) {
+//                return false;
+//            }
+//            SleepTaskServer.getSleepHandler(getActivity()).sendEmptyMessage(0x11);
+//            return true;
+//        });
+//        getDialog().getWindow().getDecorView().setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                LogUtil.d("wuliang", "dialogFragment OnTouchListener");
+//                return false;
+//            }
+//        });
     }
 
+    @Override
+    public void onFrameLayoutTouched() {
+        LogUtil.d("wuliang", " getView dialogFragment OnTouchListener");
+        removeSleep();
+    }
+
+    private void removeSleep() {
+        if (getActivity() instanceof SleepActivity) {
+            return;
+        }
+        SleepTaskServer.getSleepHandler(getActivity()).sendEmptyMessage(0x11);
+    }
+
+    @Override
+    public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+        removeSleep();
+        return false;
+    }
 }
